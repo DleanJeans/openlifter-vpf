5e2f0f65f13ccb8ee82f2449bd53a48a2607c5e7
Dlean <DleanJeans@gmail.com>
2020-11-20 01:56:36 +0700

    First Build

---
+++ precache-manifest.f9ede575c18d9499b01dc8650ef90392.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84503f4803a2483fa96a",
+    "url": "/openlifter-vpf/static/js/runtime~main.84503f48.js"
+  },
+  {
+    "revision": "eb219e419a434208aa43",
+    "url": "/openlifter-vpf/static/js/main.eb219e41.chunk.js"
+  },
+  {
+    "revision": "3bf6f85f745cbeab85e1",
+    "url": "/openlifter-vpf/static/js/1.3bf6f85f.chunk.js"
+  },
+  {
+    "revision": "eb219e419a434208aa43",
+    "url": "/openlifter-vpf/static/css/main.4a21d3cd.chunk.css"
+  },
+  {
+    "revision": "3bf6f85f745cbeab85e1",
+    "url": "/openlifter-vpf/static/css/1.60d1a3ef.chunk.css"
+  },
+  {
+    "revision": "b3660fa00cd9377a78a6bd541d1dc173",
+    "url": "/openlifter-vpf/index.html"
+  }
+];
